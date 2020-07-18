5672d73bbdce3a0a224fcf8e1462adda7a945ba0
syx <Mysong_syx@163.com>
2020-07-18 14:11:26 +0800

    第二次提交

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -4,4 +4,32 @@
 $.ajaxPrefilter(function (options) {
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
     options.url = 'http://ajax.frontend.itheima.net' + options.url;
+    // 统一为有权限的接口，设置 headers 请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    // 挂载complete 回调函数
+    // 不论成功还是失败，最终都会调用 complete 回调函数
+    options.complete = function (res) {
+        console.log(res);
+        // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+        if(!responseJSON) {
+            console.log('此时出现没有responseJSON 我们做一个错误判断');
+            let jsondata = JSON.parse(res.responseText);
+            if (jsondata.message === '身份认证失败！' && jsondata.status === 1) {
+                // 1.强制清空token
+                localStorage.removeItem('token');
+                // 2.强制跳转到登录页面
+                location.href = 'login.html';
+            }
+        }
+        if (res.responseJSON.message === '身份认证失败！' && res.responseJSON.status === 1) {
+             // 1.强制清空token
+            localStorage.removeItem('token');
+            // 2.强制跳转到登录页面
+            location.href = 'login.html';
+        }
+    }
 })
